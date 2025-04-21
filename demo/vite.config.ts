dbff9748653f52f9177c9d6c43d6807dd399b4fb
Jaafoura <73950268+J43fura@users.noreply.github.com>
2025-04-21 22:09:10 +0100

    demo: remove "vite-plugin-vue-devtools"

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,11 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 import path from 'node:path'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
